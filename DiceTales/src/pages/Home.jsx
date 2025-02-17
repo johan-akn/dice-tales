5bdaba9d00afe0005c6a97916c72aa66a21c41b8
johan-akn <johan.akn17@gmail.com>
2025-02-17 14:46:09 -0300

    clone

---
+++ DiceTales/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Dice from '../components/Dice.jsx'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+        <div className="home-container">
+            <Dice />
+        </div>
+    </div>
+  )
+}
+
+export default Home
